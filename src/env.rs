e21bb23d63c5187a4191f39a7b8e364c4b18a7da
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-16 21:51:16 +0200

    chore: Update environment configuration with additional variables

--- src/env.rs
+++ src/env.rs
@@ -2,8 +2,35 @@ use clap::Parser;
 
 #[derive(Parser, Debug, Clone)]
 pub struct Env {
-    #[clap(env)]
-    pub host: String,
-    #[clap(env)]
-    pub port: u16,
+  #[clap(env)]
+  pub host: String,
+  #[clap(env)]
+  pub port: u16,
+  #[clap(env)]
+  pub workers: usize,
+
+  #[clap(env)]
+  pub rabbitmq_url: String,
+  #[clap(env)]
+  pub rabbitmq_password: String,
+  #[clap(env)]
+  pub rabbitmq_user: String,
+  #[clap(env)]
+  pub rabbitmq_port: u16,
+
+  #[clap(env)]
+  pub postgres_url: String,
+  #[clap(env)]
+  pub postgres_password: String,
+  #[clap(env)]
+  pub postgres_user: String,
+  #[clap(env)]
+  pub postgres_port: u16,
+  #[clap(env)]
+  pub postgres_db: String,
+
+  #[clap(env)]
+  pub medtronic_username: String,
+  #[clap(env)]
+  pub medtronic_password: String,
 }
